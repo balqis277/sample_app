286cf3d616eb8d4cc7af4d6fbd748ffc3ca22414
balqis277 <balqisghazi27@gmail.com>
2021-09-01 00:19:42 +0800

    Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/help'
-
+  get 'static_pages/contact'
   get "/signup", to: "users#new"
-  resources :users
+  post "/signup", to: "users#create"
+  resources :users, only: %i(new create)
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
